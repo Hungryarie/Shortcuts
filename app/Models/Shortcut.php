fc8f467678a8ac369fbff3bf23a6d59d296008f2
hungryarie <arnoudzwakenberg@gmail.com>
2021-05-10 19:32:07 +0000

    make the relationship working. fixed typo (catagory_id)

--- app/Models/Shortcut.php
+++ app/Models/Shortcut.php
@@ -10,24 +10,25 @@ class Shortcut extends Model
     use HasFactory;
 
 
-    protected $fillable = ['url', 'name', 'remark', 'category', 'catagory_id'];
+    protected $fillable = ['url', 'name', 'remark', 'category_id'];
 
-    public function categoryname()
+    public function category()
     {
         return $this->belongsTo(Category::class);
     }
 
     public function style()
     {
+        //dd($this->category->name);
         //return $this->category;
         //return 'style4';
-        if ($this->category=='SmartPlugs'){
+        if ($this->category->name=='smartplugs'){
             return 'style2';
         }
-        if ($this->category=='Intern'){
+        if ($this->category->name=='internal'){
             return 'style3';
         }
-        if ($this->category=='Extern'){
+        if ($this->category->name=='external'){
             return 'style4';
         }
         return 'style1';
@@ -35,13 +36,13 @@ class Shortcut extends Model
     public function bgpicture()
     {
         //return $this->category;
-        if ($this->category=='SmartPlugs'){
+        if ($this->category->name=='smartplugs'){
             return 'pic02.jpg';
         }
-        if ($this->category=='Intern'){
+        if ($this->category->name=='internal'){
             return 'pic07.jpg';
         }
-        if ($this->category=='Extern'){
+        if ($this->category->name=='external'){
             return 'pic09.jpg';
         }
         return 'pic01.jpg';
