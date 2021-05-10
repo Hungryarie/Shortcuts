bef2d675f7268b679e9862230664bdcf86d35f14
hungryarie <arnoudzwakenberg@gmail.com>
2021-05-10 19:33:08 +0000

    added console items to quicly add some data

--- routes/console.php
+++ routes/console.php
@@ -17,3 +17,24 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Artisan::command('fill_shortcuts {count}', function ($count) {
+    // Create a couple App\Models\Shortcut instances...
+    $shortcuts = App\Models\Shortcut::factory()->count($count)->create();
+    print('created ' . $count . ' shortcuts instances' . PHP_EOL);
+})->purpose('fill database with shortcuts');
+
+Artisan::command('create_categories', function () {
+    // Create a three App\Models\Category instances...
+    App\Models\Category::factory()->create(['name'=>'smartplug']);
+    App\Models\Category::factory()->create(['name'=>'internal']);
+    App\Models\Category::factory()->create(['name'=>'external']);
+
+    print('created 3 category instances' . PHP_EOL);
+})->purpose('create the 3 main categories in the database');
+
+Artisan::command('test {number}', function ($number) {
+    // testing with arguments
+    print ($number . PHP_EOL);
+})->purpose('testing the console');
